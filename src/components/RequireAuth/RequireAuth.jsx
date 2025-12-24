d5231a4970c491698f922dcaebd3dd38bf091de6
v-georgakopoulos <v.georgakopoulos.dev@outlook.com>
2025-12-24 17:48:31 +0200

    added require auth component

--- src/components/RequireAuth/RequireAuth.jsx
+++ src/components/RequireAuth/RequireAuth.jsx
@@ -1,19 +1,24 @@
 import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../../store/user/user.selector"
+import { selectCurrentUser } from "../../store/user/user.selector";
 import { Navigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const RequireAuth = ({ children }) => {
-
   const location = useLocation();
-  const currentUser = useSelector(selectCurrentUser)
+  const currentUser = useSelector(selectCurrentUser);
+
+  useEffect(() => {
+    if (!currentUser) {
+      toast.warning("Please log in or sign up to access this feature.");
+    }
+  }, [currentUser]);
 
   if (!currentUser) {
-    toast.warning("Please log in or sign up to access this feature.")
     return <Navigate to="/auth" replace state={{ from: location }} />;
   }
 
   return children;
-}
+};
 
 export default RequireAuth;
